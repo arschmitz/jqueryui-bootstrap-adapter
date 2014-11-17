b71e9f1d2e33196d045473dd4b92aa650cb688da
Alexander Schmitz <arschmitz@gmail.com>
2014-11-17 13:24:17 -0500

    Fix syntax highlighting in demos to highlight js

--- demo/code_sample.js
+++ demo/code_sample.js
@@ -3,7 +3,7 @@ $(function(){
 	$( ".sample-container" ).each(function(){
 		var html = $( this ).html().replace( /</g, "&lt;" ).replace( />/g, "&gt;" );
 
-		$( this ).after( "<div class='sample-accordion'><h4 class='btn btn-info'>View Source</h4><div><pre class='brush: php; html-script: true'>" + html + "</pre></div></div>" );
+		$( this ).after( "<div class='sample-accordion'><h4 class='btn btn-info'>View Source</h4><div><pre class='brush: js; html-script: true'>" + html + "</pre></div></div>" );
 	});
 	$( ".sample-accordion" ).accordion({
 		active: false,
