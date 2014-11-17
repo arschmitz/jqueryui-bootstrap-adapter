74352983e1842a2fbc5ae5e709a90d116dee0899
Alexander Schmitz <arschmitz@gmail.com>
2014-11-17 09:05:09 -0500

    Update additional demos to add documentation

--- demo/demo.js
+++ demo/demo.js
@@ -1,47 +1,4 @@
 $(function(){
-  
-  // Accordions are a bit complicated because of substantial markup
-  // diferences. We need to make each pane its own accordion and then
-  // link them using the beforeActivate callback
-  $( ".panel-group" ).find( ".panel" ).accordion({
-    collapsible: true,
-    active: "false",
-    beforeActivate: function( e , ui ){
-      if( ui.newPanel.length > 0 ){
-        $( this ).parent().children().not( this ).accordion( "option", "active", false );
-      }
-    }
-  }).eq( 0 ).accordion( "option", "active", 0 );
-  
-  // Instantiate UI Tooltip
-  $( document ).tooltip({
-    show: false,
-    position: {
-      my: "center bottom",
-      at: "center top",
-      using: function( position, feedback ) {
-          $( this ).css( position );
-          $( "<div>" )
-            .addClass( "tooltip-arrow" )
-            .appendTo( this );
-      }
-    }
-  });
-  
-  // Instantiate UI Dialog
-  $( "#dialog" ).dialog({
-    autoOpen: false,
-    buttons: [
-      {
-        text: "foo",
-        "class": "btn-primary",
-        click: $.noop
-      }
-    ]
-  });
-  $( "#open-dialog" ).button().click(function(){
-    $( "#dialog" ).dialog( "open" );
-  });
   $( "#progressbar" ).progressbar({
     value: 67,
     classes: {
@@ -58,5 +15,8 @@ $(function(){
   $( ".scroll-link" ).waypoint(function(){
     $( "nav .active" ).removeClass( "active" );
     $( "[href$='" + this.id + "']" ).parent().addClass( "active" );
+  });
+  $( ".navbar-toggle" ).click(function(){
+    $( "#navbar" ).toggleClass( "in" );
   });
 });
